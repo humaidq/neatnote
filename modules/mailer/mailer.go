1ba0c1021c2c9ffe285c7f79eafc1b2e531ea536
Humaid AlQassimi <me@humaidq.ae>
2020-01-12 11:36:13 +0000

    Use Site Name and SMTP from configuration for mailer.

--- modules/mailer/mailer.go
+++ modules/mailer/mailer.go
@@ -18,6 +18,7 @@ package mailer
 import (
 	"git.sr.ht/~humaid/neatnote/modules/settings"
 	"net/smtp"
+	"strings"
 )
 
 // EmailCode emails the provided code to the provided email address.
@@ -25,13 +26,13 @@ func EmailCode(to string, code string) (err error) {
 	from := settings.Config.EmailAddress
 	message := "From: <" + from + ">\n" +
 		"To: <" + to + ">\n" +
-		"Subject: Neat Note login code\n\n" +
+		"Subject: " + settings.Config.SiteName + " login code\n\n" +
 		"Hello!\nYour login code is " + code + "\n\n" +
 		"Ignore this message if you have not requested a login.\n\n\n" +
-		"- Neat Note\nThis message is sent from an unmonitored inbox."
+		"- " + settings.Config.SiteName + "\nThis message is sent from an unmonitored inbox."
 
 	err = smtp.SendMail(settings.Config.EmailSMTPServer,
-		smtp.PlainAuth("", from, settings.Config.EmailPassword, "smtp.migadu.com"),
+		smtp.PlainAuth("", from, settings.Config.EmailPassword, strings.Split(settings.Config.EmailSMTPServer, ":")[0]),
 		from, []string{to}, []byte(message))
 
 	return err
